511b4a39f3cdc18be81dc27ec46e96ee196cd55a
Nyew62 <nyew@umich.edu>
2025-04-09 03:16:09 -0400

    Add files via upload

---
+++ SocialMediaAccount.h
@@ -0,0 +1,13 @@
+#ifndef NYEW_HW4_Q1_SOCIALMEDIAACCOUNT_H
+#define NYEW_HW4_Q1_SOCIALMEDIAACCOUNT_H
+
+#include <iostream>
+using namespace std;
+template <class A>
+class SocialMediaAccount{
+public:
+private:
+
+};
+
+#endif //NYEW_HW4_Q1_SOCIALMEDIAACCOUNT_H
---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+** Author : A'Nyla Weathers
+** Program : hw4, q1
+** Date Created : April 8, 2025
+** Date Last Modified : April 8, 2025
+** Usage : No command line arguments
+**
+** Problem:
+
+
+
+
+
+
+
+*************************************************************************/
+
+#include <iostream>
+#include "SocialMediaAccount.h"
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
+
